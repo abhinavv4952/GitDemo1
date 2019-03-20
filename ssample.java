d9210ed42cb986c2967b2a68dc75411314aca03f
abhinavv4952 <48243184+abhinavv4952@users.noreply.github.com>
2019-03-20 12:01:59 +0530

    Update ssample.java

--- ssample.java
+++ ssample.java
@@ -1,26 +1,20 @@
+import java.sql.ResultSet;
 import java.util.*;
-public class Main
-{
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    StringBuilder s=new StringBuilder();
-    s=excel(n,s);
-    System.out.println(s.reverse());
-}
-public static StringBuilder excel(int n,StringBuilder s){
-    int rem=n%26;
-    if(rem==0){
-        s.append("Z");
-        n=n/26-1;
-    }
-    else{
-        s.append((char)(rem-1+'A'));
-        n=n/26;
-    }
-    if(n!=0){
-        excel(n,s);
-    }
-    return s;
-}
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<DBCol> al=new ArrayList<>();
+		int i,j,k,l;
+		ResultSet rs=stmt.executeQuery();//create a statement of select statement with stmt variable
+		while(rs.next()){
+			DBCol db=new DBCol();
+			db.setAttr1(rs.getString(1));
+			db.setAttr2(rs.getInt(1));
+			db.setAttr3(rs.getString(1));
+			al.add(db);
+		}
+		//print the arraylist
+	}
+
 }
